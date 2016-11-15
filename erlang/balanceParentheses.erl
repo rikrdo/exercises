24bb68f8ec88c90c8c29851f2b00851ccefbc42e
rikrdo <islasi.ricardo@gmail.com>
2016-11-15 11:17:52 -0600

    Balanced Parentheses

---
+++ erlang/balanceParentheses.erl
@@ -0,0 +1,55 @@
+%%%-------------------------------------------------------------------
+%%% @author Richard
+%%% @copyright (C) 2016, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 09. nov 2016 00:07
+%%%-------------------------------------------------------------------
+-module(balanceParentheses).
+-author("Richard").
+
+%% API
+-export([run/1]).
+
+run(TestString)->
+	{analize(TestString,[]),TestString}.
+
+analize([],Evaluate) ->
+				if
+					Evaluate==[] ->
+						true;
+					true ->
+						false
+				end
+;
+analize([H|Tail],Evaluate) ->
+				if
+					(H==40) or (H==60) or (H==91) or (H==123) ->
+						analize(Tail,[H|Evaluate]);
+					H==41 ->
+						checkOpen(Evaluate,Tail,40);
+					H==62 ->
+						checkOpen(Evaluate,Tail,60);
+					H==93 ->
+						checkOpen(Evaluate,Tail,91);
+					H==125 ->
+						checkOpen(Evaluate,Tail,123);
+					true ->
+						analize(Tail,Evaluate)
+				end.
+
+checkOpen(Evaluate,Tail,Open)->
+	if
+		Evaluate==[] ->
+			false;
+		true->
+			case head(Evaluate) of
+				Open ->
+					analize(Tail,(tl(Evaluate)));
+				_ ->
+					false
+			end
+	end.
+
+head([H|_])	->	H.
